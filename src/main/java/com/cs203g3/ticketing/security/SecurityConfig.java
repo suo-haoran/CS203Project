452777043e847644a6869304902192299b450e03
Yanagi57 <yanagisen@outlook.com>
2023-09-17 01:48:38 +0800

    Removed test endpoint

--- src/main/java/com/cs203g3/ticketing/security/SecurityConfig.java
+++ src/main/java/com/cs203g3/ticketing/security/SecurityConfig.java
@@ -57,7 +57,6 @@ public class SecurityConfig {
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> 
                 auth.requestMatchers("/api/auth/**").permitAll()
-                    .requestMatchers("/api/test/test").permitAll()
                     .requestMatchers("/api/test/**").permitAll()
                     .anyRequest().authenticated()
             );
