221ac39390b31614fd1542a2f788a03183968b58
Yanagi57 <yanagisen@outlook.com>
2023-09-23 23:10:50 +0800

    Removed moderator role.

--- src/main/java/com/cs203g3/ticketing/security/auth/AuthService.java
+++ src/main/java/com/cs203g3/ticketing/security/auth/AuthService.java
@@ -77,25 +77,17 @@ public class AuthService {
                     .orElseThrow(() -> new ResourceNotFoundException(Role.class, (long) ERole.ROLE_USER.ordinal()));
             roles.add(userRole);
         } else {
-            strRoles.forEach(role -> {
-                switch (role) {
-                    case "admin":
-                        Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
-                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-                        roles.add(adminRole);
-
-                        break;
-                    case "mod":
-                        Role modRole = roleRepository.findByName(ERole.ROLE_MODERATOR)
-                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-                        roles.add(modRole);
-
-                        break;
-                    default:
-                        Role userRole = roleRepository.findByName(ERole.ROLE_USER)
-                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-                        roles.add(userRole);
+            strRoles.forEach(roleStr -> {
+                Role role = null;
+                if (roleStr.equals("user")) {
+                    role = roleRepository.findByName(ERole.ROLE_ADMIN)
+                            .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                } else {
+                    role = roleRepository.findByName(ERole.ROLE_USER)
+                            .orElseThrow(
+                                    () -> new ResourceNotFoundException(Role.class, (long) ERole.ROLE_USER.ordinal()));
                 }
+                roles.add(role);
             });
         }
 
--- src/main/java/com/cs203g3/ticketing/user/ERole.java
+++ src/main/java/com/cs203g3/ticketing/user/ERole.java
@@ -2,6 +2,5 @@ package com.cs203g3.ticketing.user;
 
 public enum ERole {
     ROLE_USER,
-    ROLE_MODERATOR,
     ROLE_ADMIN
 }
