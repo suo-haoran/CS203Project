cc85810c69f6166e67a94871cac4e8f1b8e51952
kahliya <alex.c0304@hotmail.com>
2023-10-24 16:05:46 +0800

    Implement query param to filter sections by categoryId

--- src/main/java/com/cs203g3/ticketing/section/SectionRepository.java
+++ src/main/java/com/cs203g3/ticketing/section/SectionRepository.java
@@ -5,10 +5,12 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.cs203g3.ticketing.category.Category;
 import com.cs203g3.ticketing.venue.Venue;
 
 
 public interface SectionRepository extends JpaRepository<Section, Long>{
+    List<Section> findAllSectionsByCategory(Category category);
     List<Section> findAllSectionsByCategoryVenue(Venue venue);
     Optional<Section> findByCategoryVenueAndId(Venue venue, Long sectionId);
 }
--- src/main/java/com/cs203g3/ticketing/section/SectionService.java
+++ src/main/java/com/cs203g3/ticketing/section/SectionService.java
@@ -6,6 +6,8 @@ import java.util.stream.Collectors;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import com.cs203g3.ticketing.category.Category;
+import com.cs203g3.ticketing.category.CategoryRepository;
 import com.cs203g3.ticketing.exception.ResourceNotFoundException;
 import com.cs203g3.ticketing.section.dto.SectionResponseDto;
 import com.cs203g3.ticketing.venue.Venue;
@@ -16,26 +18,40 @@ public class SectionService {
     
     private ModelMapper modelMapper;
 
+    private CategoryRepository categories;
     private SectionRepository sections;
     private VenueRepository venues;
     
-    public SectionService(ModelMapper modelMapper, SectionRepository sections, VenueRepository venues) {
+    public SectionService(ModelMapper modelMapper, CategoryRepository categories, SectionRepository sections, VenueRepository venues) {
         this.modelMapper = modelMapper;
-
+        this.categories = categories;
         this.sections = sections;
         this.venues = venues;
     }
 
+    public Venue getVenueThrowExceptionIfNotExists(Long venueId) {
+        return venues.findById(venueId).orElseThrow(() -> new ResourceNotFoundException(Venue.class, venueId));
+    }
+
     public List<SectionResponseDto> getAllSectionsByVenueId(Long venueId) {
-        Venue venue = venues.findById(venueId).orElseThrow(() -> new ResourceNotFoundException(Venue.class, venueId));
+        Venue venue = getVenueThrowExceptionIfNotExists(venueId);
 
         return sections.findAllSectionsByCategoryVenue(venue).stream()
             .map(section -> modelMapper.map(section, SectionResponseDto.class))
             .collect(Collectors.toList());
     }
 
+    public List<SectionResponseDto> getAllSectionsByVenueIdAndCategoryId(Long venueId, Long categoryId) {
+        Venue venue = getVenueThrowExceptionIfNotExists(venueId);
+        Category category = categories.findByVenueAndId(venue, categoryId).orElseThrow(() -> new ResourceNotFoundException(Category.class, categoryId));
+
+        return sections.findAllSectionsByCategory(category).stream()
+            .map(section -> modelMapper.map(section, SectionResponseDto.class))
+            .collect(Collectors.toList());
+    }
+
     public SectionResponseDto getSectionByVenueIdAndSectionId(Long venueId, Long sectionId) {
-        Venue venue = venues.findById(venueId).orElseThrow(() -> new ResourceNotFoundException(Venue.class, venueId));
+        Venue venue = getVenueThrowExceptionIfNotExists(venueId);
 
         return sections.findByCategoryVenueAndId(venue, sectionId)
             .map(section -> modelMapper.map(section, SectionResponseDto.class))
